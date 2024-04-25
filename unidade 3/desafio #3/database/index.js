9d68c05242da0a12c7fabb2dc514b5a26da50bc8
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-04-25 01:45:25 -0300

    chore: add database config

---
+++ unidade 3/desafio #3/database/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+      dialect: 'postgres',
+      host: 'localhost',
+      username: 'postgres',
+      password: '123',
+      database: 'consultorio',
+      define: {
+        timestamps: true
+      }
+    
+  };
+  
+
+  
